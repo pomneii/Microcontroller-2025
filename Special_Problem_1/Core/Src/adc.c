dfd65651ef9c09312a4adccead4eca2b94f9dfc1
pomneii <pomneii75@gmail.com>
2025-09-09 12:29:53 +0700

    finish special problem 1😁

--- Special_Problem_1/Core/Src/adc.c
+++ Special_Problem_1/Core/Src/adc.c
@@ -64,7 +64,7 @@ void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_3;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_15CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
